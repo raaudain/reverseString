453148d3511fe49df81839eec5f0e188ca678bd0
Ramon Audain <raudain123@gmail.com>
2020-05-13 09:19:57 -0700

    Add Solution #3

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 
 function reverseString(str) {
-  // // Solution #1
-  // return str.split("").reverse().join("");
+  // Solution #1
+  return str.split("").reverse().join("");
 
-  // // Solution #2
-  // let reversed = "";
+  
+  // Solution #2
+  let reversed = "";
 
-  // for (let character of str) {
-  //   reversed = character + reversed;
-  //   //console.log(reversed)
-  // }
+  for (let character of str) {
+    reversed = character + reversed;
+    //console.log(reversed)
+  }
 
   return reversed
+  
+  
+  //Solution #3
+  return str.split("").reduce((reversed, character) => character + reversed, "");
 }
 
 
